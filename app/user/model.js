46dc36fbc8b0d2e904ea3e5b4869896b31cfd5bb
danielalbertjeffery <daniel.albert.jeffery@gmail.com>
2016-05-22 13:19:07 +0800

    Just the styling and basic layout

---
+++ app/user/model.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  username: DS.attr('string'),
+  numberOfChirps: DS.attr('number'),
+  numberOfFollowing: DS.attr('number'),
+  numberOfFollowers: DS.attr('number'),
+  // New attributes:
+  aboutMe: DS.attr('string'),
+  joinedAt: DS.attr('date'),
+  chirps: DS.hasMany('chirp', { async: true })
+});
